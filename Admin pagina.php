93fd52c0f57d7dfad6ccff2272083fe2a507ab85
SanderTJ <sandertjoelker2000@gmail.com>
2022-11-28 15:20:54 +0100

    Create Admin pagina.php

---
+++ Admin pagina.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+ <html lang="nl">
+<head>
+    <title>Admin</title>
+    <style>
+.wrapper {
+    border: 2px solid;
+    height: 720px;
+    width: 900px;
+    border-bottom-left-radius: 25px; 
+    border-top-left-radius: 25px;
+    border-bottom-right-radius: 25px;
+    border-top-right-radius: 25px;
+    margin: 0 auto;
+}
+.testing {
+    font-size: x-large;
+    padding-left: 200px;
+} 
+h1 {
+    text-align: center;
+}
+        </style>
+</head>
+
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Bestellen.db";
+
+?>
+
+<body>
+    <div class="wrapper">
+        <h1>Admin</h1>
+        <div class="testing">
+            <p>Klanten</p>
+            <?php
+
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                // Check connection
+                if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT * FROM user";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "id: " . $row["ID"]. " - Naam: " . $row["Naam"]. " - Email: " . $row["Email"]. " "  ."<br>";
+                }
+                } else {
+                echo "0 results";
+                }
+
+                mysqli_close($conn);
+
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                // Check connection
+                if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT * FROM inloggen";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "id: " . $row["ID"]. " - Naam: " . $row["Naam"]. "- Email " . $row["Email"]. " - password_hash: " . $row["password_hash"].  "<br>";
+                }
+                } else {
+                echo "0 results";
+                }
+
+                mysqli_close($conn);
+                ?>
+        </div>
+    </div>
+</body>
+</html>
